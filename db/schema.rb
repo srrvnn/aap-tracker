2c28a3eb9434a4f8d5194686778c7507ce816254
= <=>
2015-10-19 13:14:01 -0700

    added categories schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151019004302) do
+ActiveRecord::Schema.define(version: 20151019200926) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.integer  "sno"
