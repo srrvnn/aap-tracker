a364c6f32245ec060a629e24bbc0ebcf130f9a76
gracew2 <grace.wu@gmail.com>
2015-10-26 12:46:23 -0700

    added new routes

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -4,20 +4,29 @@ class ProjectController < ApplicationController
   end
 
   def new
-
+  	@project = Project.new
+  	# retrieve data from form
   end
+  
+  def create
+  	
+  end
+
 
   # view a single project detail page
   def view
-
+  	@project = Project.find(params[:id])
+  	#TODO: add a form
   end
 
   def update
-
+  	@project = Project.find(params[:id])
+  	# TODO: 
   end
 
   def delete
-
+  	Project.find(params[:id]).destroy
+  	redirect_to action: "index"
   end
 
 end
