226dfedd6f7dc8ab893b686139e44113090ec758
Michelle Guo <michelleguo@DN0a23746c.SUNet>
2015-11-06 13:16:57 -0800

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151019201907) do
+ActiveRecord::Schema.define(version: 20151106205914) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -33,10 +33,27 @@ ActiveRecord::Schema.define(version: 20151019201907) do
     t.string   "title"
     t.string   "url"
     t.text     "description"
-    t.integer  "project_id"
     t.datetime "last_updated"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
