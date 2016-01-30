0dd09e0180332d29d27e69610167f201158a980d
Saravanan Ganesh <saravanan.ganesh@ensighten.com>
2016-01-30 11:54:18 -0800

    add facebook user managed groups to find friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'pp'
+
 class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -7,6 +9,7 @@ class User < ActiveRecord::Base
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
+      pp auth
     end
   end
 end
