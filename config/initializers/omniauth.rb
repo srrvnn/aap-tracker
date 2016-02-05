a5c8df25debbcb03c78689a9b8ad4b053ba98bbc
Saravanan Ganesh <saravanan.ganesh@ensighten.com>
2016-02-05 02:25:48 -0800

    remove user_managed_groups from facebook api requests

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, Rails.application.secrets.fb_app_id, Rails.application.secrets.fb_app_secret,
-  		:scope => 'email, user_friends, user_managed_groups', :image_size => 'normal'
+  		:scope => 'email, user_friends', :image_size => 'normal'
 end
